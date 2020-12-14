520527c129e1bb8a159d8a1c3431fda2f517f7bf
m_a_ks_ <maksim.m00@mail.ru>
2020-12-14 22:01:33 +0300

    .

--- miet_lab8.2_client/src/Main.java
+++ miet_lab8.2_client/src/Main.java
@@ -1,34 +1,80 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.*;
+import java.net.Socket;
 
 public class Main {
     static JFrame frame;
-    static JTextField textFieldHost;
-    static JTextField textFieldPort;
+    static JTextField textFieldAddress;
     static JButton buttonConnect;
+    static JButton buttonDownload;
 
-    public static void connect(){
-        Socket socket = null;
-        try {//получение строки клиентом
-            socket = new Socket("localhost", 20);
-            BufferedReader dis = new BufferedReader(new
-                    InputStreamReader(socket.getInputStream()));
-            String msg = dis.readLine();
-            System.out.println(msg);
-        } catch (IOException e) {
+    static Socket socket = null;
+
+    public static void connect() {
+        try {
+            String[] uri = textFieldAddress.getText().split(":");
+            socket = new Socket(uri[0], Integer.parseInt(uri[1]));
+            buttonConnect.setText("disconnect");
+            textFieldAddress.setEditable(false);
+            buttonDownload.setVisible(true);
+        } catch (Exception e) {
             System.out.println("ошибка: " + e);
         }
     }
 
-    public static void runAndListen(){
+    public static void disconnect() {
+        try {
+            socket.close();
+            buttonConnect.setText("connect");
+            textFieldAddress.setEditable(true);
+            buttonDownload.setVisible(false);
+        } catch (Exception e) {
+            System.out.println("ошибка: " + e);
+        }
+    }
+
+    public static void downloadFile() {
+        try {
+            /*BufferedReader dis = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String msg = dis.readLine();
+            System.out.println(msg);*/
+
+            BufferedOutputStream bis = new BufferedOutputStream(new Output("somefile.dat"));
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+            byte[] byteArray = new byte[8192];
+            int in;
+            while ((in = bis.read(byteArray)) != -1){
+                bos.write(byteArray,0,in);
+            }
+            bis.close();
+            bos.close();
+
+            connect();
+        }
+        catch (Exception e){
+            System.out.println("ошибка: " + e);
+        }
+    }
+
+    public static void runAndListen() {
         buttonConnect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                connect();
+                if (buttonConnect.getText().equals("connect")) {
+                    connect();
+                } else {
+                    disconnect();
+                }
+            }
+        });
+        buttonDownload.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(textFieldAddress.getText().equals("127.0.0.1:20") || textFieldAddress.getText().equals("localhost:20")){
+                    downloadFile();
+                }
             }
         });
     }
@@ -38,23 +84,23 @@ public class Main {
         frame.setSize(200, 200);
         frame.setResizable(false);
 
-        textFieldHost = new JTextField("host");
-        textFieldHost.setBounds(20,20,100,20);
-        textFieldHost.setVisible(true);
-
-        textFieldPort = new JTextField("port");
-        textFieldPort.setBounds(textFieldHost.getX()+textFieldHost.getWidth()+5,20,50,20);
-        textFieldPort.setVisible(true);
+        textFieldAddress = new JTextField("127.0.0.1:0");
+        textFieldAddress.setBounds(20, 20, 150, 20);
+        textFieldAddress.setVisible(true);
 
         buttonConnect = new JButton("connect");
-        buttonConnect.setBounds(textFieldHost.getX(), textFieldHost.getY()+textFieldHost.getHeight()+10,100,30);
+        buttonConnect.setBounds(textFieldAddress.getX(), textFieldAddress.getY() + textFieldAddress.getHeight() + 10, 150, 30);
         buttonConnect.setVisible(true);
+
+        buttonDownload = new JButton("download file");
+        buttonDownload.setBounds(textFieldAddress.getX(), textFieldAddress.getY() + textFieldAddress.getHeight() + 70, 150, 30);
+        buttonDownload.setVisible(false);
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
-        panel.add(textFieldHost);
-        panel.add(textFieldPort);
+        panel.add(textFieldAddress);
         panel.add(buttonConnect);
+        panel.add(buttonDownload);
 
         frame.setContentPane(panel);
         frame.setVisible(true);
--- miet_lab8.2_server/src/Main.java
+++ miet_lab8.2_server/src/Main.java
@@ -7,15 +7,25 @@ public class Main{
         Socket socket = null;
 
         System.out.println("Server start");
-        try {//посылка строки клиенту
+        try {
             ServerSocket server = new ServerSocket(20);
             while(true)
             {
                 socket = server.accept();
-                PrintStream ps = new PrintStream(socket.getOutputStream());
-                ps.println("привет!"+(count++));
-                ps.flush();
-                socket.close(); // разрыв соединения
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream("somefile.dat"));
+                BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+                byte[] byteArray = new byte[8192];
+                int in;
+                while ((in = bis.read(byteArray)) != -1){
+                    bos.write(byteArray,0,in);
+                }
+                bis.close();
+                bos.close();
+                //PrintStream ps = new PrintStream(socket.getOutputStream());
+                //ps.println("привет!"+(count++));
+                //ps.flush();
+
+                socket.close();
             }
 
         }catch (IOException e){System.out.println("ошибка: " + e); }
